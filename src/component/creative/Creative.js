b40a125409c3ebaf532d0e3fc14b3d50728fb3bb
gopalvaghela <vaghelagopal160@gmail.com>
2024-02-02 18:59:51 +0530

    props

---
+++ src/component/creative/Creative.js
@@ -0,0 +1,59 @@
+import './creative.css';
+
+
+function Creative(props) {
+    return (
+        <div className="cc-center">
+            <div className="container">
+                <div className="cc-tital">
+                    <span>READ OUR DIFFERNCE</span>
+                    <h2>WHY CHOOSE CREATIVE</h2>
+                </div>
+                <div className="cc-info d-flex">
+
+                    {
+                        props.w_box.map((ele) => {
+                            return (
+                                <div className="cc-box">
+                                    <div className="img" >
+                                        <img src={ele.boxImg} id={ele.pid} style={{ backgroundColor: "#F6475F" }} alt="" />
+                                    </div>
+                                    <div className="desc">
+                                        <h5>{ele.tital}</h5>
+                                        <p>{ele.subtital}</p>
+                                    </div>
+                                </div>
+                            );
+                        })
+                    }
+                </div>
+            </div>
+       
+        </div>
+
+
+
+    );
+}
+export default Creative;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
